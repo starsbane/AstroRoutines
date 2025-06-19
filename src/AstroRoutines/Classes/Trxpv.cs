6062cde56cbda8737bcabf289894a5b01abfdad9
Alex Man <alexthemans@gmail.com>
2025-06-19 16:15:57 +0800

    Logic of Trxpv should follow original source as much as possible

--- src/AstroRoutines/Classes/Trxpv.cs
+++ src/AstroRoutines/Classes/Trxpv.cs
@@ -5,22 +5,18 @@ namespace AstroRoutines
         /// <summary>
         /// Multiply a pv-vector by the transpose of an r-matrix.
         /// </summary>
-        public static void Trxpv(double[,] r, double[,] pv, double[,] trpv)
+        /// <param name="r">R-matrix</param>
+        /// <param name="pv">PV-vector</param>
+        /// <param name="trpv">Resulting transposed PV-vector</param>
+        public static void Trxpv(double[,] r, double[,] pv, ref double[,] trpv)
         {
-            double[] p = new double[3] { pv[0, 0], pv[0, 1], pv[0, 2] };
-            double[] v = new double[3] { pv[1, 0], pv[1, 1], pv[1, 2] };
-            double[] rtp = new double[3];
-            double[] rtv = new double[3];
+            double[,] tr = new double[3, 3];
 
-            // Transpose multiply position
-            for (int j = 0; j < 3; j++)
-            {
-                rtp[j] = r[0, j] * p[0] + r[1, j] * p[1] + r[2, j] * p[2];
-                rtv[j] = r[0, j] * v[0] + r[1, j] * v[1] + r[2, j] * v[2];
-            }
+            // Transpose of matrix r
+            Tr(r, ref tr);
 
-            trpv[0, 0] = rtp[0]; trpv[0, 1] = rtp[1]; trpv[0, 2] = rtp[2];
-            trpv[1, 0] = rtv[0]; trpv[1, 1] = rtv[1]; trpv[1, 2] = rtv[2];
+            // Matrix tr * vector pv -> vector trpv
+            Rxpv(tr, pv, ref trpv);
         }
     }
 }
